6b2240fd05e869ef35e5777eb357f851bdc6078d
Oliver Q.R <qvdoli@gmail.com>
2024-10-09 14:25:35 +0200

    Temperature graphs made

--- 2_Scripts/ACE weather data analysis.R
+++ 2_Scripts/ACE weather data analysis.R
@@ -160,7 +160,7 @@ ace_data %>%
                           .default = week),
          tmax_cat = cut(tmax, c(0, 30, 34, 37, 40, max(tmax)))) %>%
   ggplot(aes(x = weekday, y = -week, fill = tmax_cat)) +
-  geom_tile(colour = "white", size = 0.4) +
+  geom_tile(colour = "white", linewidth = 0.4) +
   geom_text(aes(label = day), size = 2.5) +
   scale_fill_manual(values = c("white", "#ffffcc", "yellow", "orange", "red"),
                     labels = c("≤30°C", "30-34°C", "34-37°C", "37-40°C", ">40°C"),
@@ -185,7 +185,6 @@ ace_data %>%
         legend.position = "right",
         legend.text = element_text(hjust = 0.5),
         legend.title = element_text(face = "bold", size = 9, hjust = 0.5),
-        legend.margin = margin(l = 10),
         panel.border = element_rect(colour = "grey", fill = NA, linewidth = 1)) +
   facet_wrap(vars(month), nrow = 4, ncol = 3, scales = "free")
 
@@ -223,7 +222,7 @@ ace_data %>%
               ),
               align = "center",
               style = list(
-                #fontWeight = "bold"
+                fontFamily = "Arial"
               )),
             columns = list(
               tmax = colDef(
@@ -243,17 +242,155 @@ ace_data %>%
                 }
               ),
               y2023 = colDef(
-                name = "2023"),
+                name = "2023",
+                cell = data_bars(
+                  .,
+                  fill_color = "darkred",
+                  max_value = 365,
+                  text_position = "outside-end"
+                )),
               ref_period = colDef(
-                name = "1973-2000"
-              )
+                name = "1973-2000",
+                cell = data_bars(
+                  .,
+                  fill_color = "darkred",
+                  max_value = 365,
+                  text_position = "outside-end"
+                ))
             ),
             theme = reactableTheme(
               borderColor = "#e0e0e0",
-              cellPadding = "8px",
-              fontFamily = "Arial"
-            ))
+              cellPadding = "8px"
+            )) %>%
+  add_source(source = html("<strong>Table 1.</strong> Number of days with high temperatures.<br>
+                           Data: Aemet."),
+             font_size = 14,
+             font_style = "italic",
+             font_color = "grey50",
+             align = "left",
+             margin = margin(t = 10))
 
+
+# Daily minimum temperatures throughout 2023
+ace_data %>%
+  filter(year == 2023) %>%
+  mutate(weekday = wday(date, label = TRUE, week_start = 1),
+         month = month(date, label = TRUE, abbr = FALSE),
+         week = isoweek(date)) %>%
+  mutate(week = case_when(month == "December" & week == 1 ~ 53,
+                          month == "January" & week %in% 52:53 ~ 0,
+                          .default = week),
+         tmin_cat = cut(tmin, c(0, 20, 25, 30, max(tmin)))) %>%
+  ggplot(aes(x = weekday, y = -week, fill = tmin_cat)) +
+  geom_tile(colour = "white", size = 0.4) +
+  geom_text(aes(label = day), size = 2.5) +
+  scale_fill_manual(values = c("white", "yellow", "orange", "red"),
+                    labels = c("≤20°C", "20-25°C", "25-30°C", ">30°C"),
+                    guide = guide_legend(title.position = "top",
+                                         override.aes = list(colour = "black"))) +
+  labs(title = "Daily minimum temperatures throughout the year 2023",
+       subtitle = "Lanzarote Airport data",
+       caption = "Data: Aemet.",
+       fill = "Tmin") +
+  theme_minimal(base_family = "Arial") +
+  theme(aspect.ratio = 1/2,
+        plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
+        plot.subtitle = element_markdown(size = 14, colour = "#5a5a5a"),
+        plot.caption = element_text(size = 10, face = "italic", colour = "grey50", vjust = -1),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.y = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(face = "bold", size = 15),
+        legend.position = "right",
+        legend.text = element_text(hjust = 0.5),
+        legend.title = element_text(face = "bold", size = 9, hjust = 0.5),
+        panel.border = element_rect(colour = "grey", fill = NA, linewidth = 1)) +
+  facet_wrap(vars(month), nrow = 4, ncol = 3, scales = "free")
+
+
+ace_data %>%
+  filter(year == 2023) %>%
+  summarise(t20minus = sum(tmin <= 20),
+            t20_25 = sum(tmin > 20 & tmin <= 25),
+            t25_30 = sum(tmin > 25 & tmin <= 30),
+            t30plus = sum(tmin > 30)) %>%
+  pivot_longer(cols = everything(), 
+               names_to = "tmin", 
+               values_to = "y2023") %>%
+  left_join(ace_data %>%
+              filter(year > 1972 & year < 2001) %>%
+              group_by(year) %>%
+              summarise(t20minus = sum(tmin <= 20),
+                        t20_25 = sum(tmin > 20 & tmin <= 25),
+                        t25_30 = sum(tmin > 25 & tmin <= 30),
+                        t30plus = sum(tmin > 30)) %>%
+              summarise(across(starts_with("t"), \(x) round(mean(x, na.rm = TRUE), 2))) %>%
+              pivot_longer(cols = everything(),
+                           names_to = "tmin",
+                           values_to = "ref_period"),
+            by = "tmin") %>%
+  reactable(fullWidth = FALSE,
+            pagination = FALSE,
+            defaultColDef = colDef(
+              headerStyle = list(
+                background = "#67000d",
+                color = "#f0f0f0",
+                align = "center",
+                fontWeight = "bold",
+                borderBottom = "1px solid #f0f0f0"
+              ),
+              align = "center",
+              style = list(
+                fontFamily = "Arial"
+              )),
+            columns = list(
+              tmin = colDef(
+                name = "Temperature",
+                minWidth = 120,
+                align = "left",
+                cell = function(value){
+                  if(value == "t20minus"){
+                    value <- "≤20°C"
+                  } else if(value == "t20_25"){
+                    value <- ">20-25°C"
+                  } else if(value == "t25_30"){
+                    value <- ">25-30°C"
+                  } else{
+                    value <- ">30°C"
+                  }
+                }
+              ),
+              y2023 = colDef(
+                name = "2023",
+                cell = data_bars(
+                  .,
+                  fill_color = "darkred",
+                  max_value = 365,
+                  text_position = "outside-end"
+                )),
+              ref_period = colDef(
+                name = "1973-2000",
+                cell = data_bars(
+                  .,
+                  fill_color = "darkred",
+                  max_value = 365,
+                  text_position = "outside-end"
+                ))
+            ),
+            theme = reactableTheme(
+              borderColor = "#e0e0e0",
+              cellPadding = "8px"
+            )) %>%
+  add_source(source = html("<strong>Table 2.</strong> Number of days according to the min. temperature.<br>
+                           Data: Aemet."),
+             font_size = 14,
+             font_style = "italic",
+             font_color = "grey50",
+             align = "left",
+             margin = margin(t = 10))
 
 
 
@@ -295,6 +432,7 @@ ace_data %>%
         panel.grid.minor = element_blank())
 
 
+
 # monthly temperature deviations
 ace_data %>%
   filter(year > 1972 & year < 2001) %>%
@@ -305,10 +443,72 @@ ace_data %>%
                group_by(month) %>%
                summarise(tmean = mean(tmean, na.rm = TRUE)) %>%
                select(month, tmean),
-             by = "month")
+             by = "month") %>%
+  mutate(tmean_diff = tmean - tmean_ref) %>%
+  mutate(across(starts_with("tmean"), ~round(., 2))) %>%
+  relocate(tmean, .before = tmean_ref) %>%
+  reactable(fullWidth = FALSE,
+            pagination = FALSE,
+            bordered = FALSE,
+            defaultColDef = colDef(
+              headerStyle = list(
+                background = "#67000d",
+                color = "#f0f0f0",
+                align = "center",
+                fontWeight = "bold",
+                borderBottom = "1px solid #f0f0f0"
+              ),
+              align = "center",
+              style = list(
+                fontFamily = "Arial"
+              )
+            ),
+            columns = list(
+              month = colDef(name = "Month",
+                             cell = function(value, index){
+                               value <- month.name[index]
+                             }),
+              tmean = colDef(name = "2023"),
+              tmean_ref = colDef(name = "1973-2000"),
+              tmean_diff = colDef(name = "difference",
+                                  style = function(value){
+                                    if(value < 0){
+                                      list(background = "#eff3ff")
+                                    } else if(value >= 0 & value < 0.5){
+                                      list(background = "white")
+                                    } else if(value >= 0.5 & value < 1){
+                                      list(background = "#fee5d9")
+                                    } else if(value >= 1 & value < 1.5){
+                                      list(background = "#fcbba1",
+                                           fontWeight = "bold")
+                                    } else if(value >= 1.5 & value < 2){
+                                      list(background = "#fc9272",
+                                           fontWeight = "bold")
+                                    } else if(value >= 2 & value < 2.5){
+                                      list(background = "#fb6a4a",
+                                           color = "white",
+                                           fontWeight = "bold")
+                                    } else if(value >= 2.5 & value < 3){
+                                      list(background = "#de2d26",
+                                           color = "white",
+                                           fontWeight = "bold")
+                                    } else {
+                                      list(background = "#a50f15",
+                                           color = "white",
+                                           fontWeight = "bold")
+                                    }
+                                  })
+            )) %>%
+  add_source(source = html("<strong>Table 3.</strong> Monthly average temperature differences.<br>
+                           Data: Aemet."),
+             font_size = 14,
+             font_style = "italic",
+             font_color = "grey50",
+             align = "left",
+             margin = margin(t = 10))
 
 
-
+# precipitation
 
 
 
