00bbfddc782b2a1aa5e7e8309a89453e361165cf
Oliver Q.R <qvdoli@gmail.com>
2024-10-11 17:44:37 +0200

    Added graphs and updated some

--- 2_Scripts/ACE weather data analysis.R
+++ 2_Scripts/ACE weather data analysis.R
@@ -29,7 +29,7 @@ ace_data %>%
        subtitle = "Data from 1973 to 2023",
        x = "Year",
        y = "Mean temperature (°C)",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_text(size = 14, colour = "#5a5a5a"),
@@ -38,6 +38,9 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+ggsave("3_Graphs/Annual temperature.png",
+       plot = last_plot())
 
 
 
@@ -80,7 +83,7 @@ ace_data %>%
        subtitle = "Comparison of temperature deviations from the reference mean (period: 1973-2020)",
        x = "Year",
        y = "Temperature (°C)",
-       caption = "Data: Aemet.",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        fill = "°C") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
@@ -92,6 +95,9 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+ggsave("3_Graphs/Temp anomalies.png",
+       plot = last_plot())
 
 
 # year 2023
@@ -135,7 +141,7 @@ ace_data %>%
        with the <span style=color:#313695>**1973-2000 reference period**</span>",
        x = "Day",
        y = "Mean temperature (°C)",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_markdown(size = 14, colour = "#5a5a5a"),
@@ -147,6 +153,9 @@ ace_data %>%
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
 
+
+ggsave("3_Graphs/Temp2023_day_by_day.png",
+       plot = last_plot())
 
 
 # maximum temperature calendar
@@ -168,7 +177,7 @@ ace_data %>%
                                          override.aes = list(colour = "black"))) +
   labs(title = "Daily maximum temperatures throughout the year 2023",
        subtitle = "Lanzarote Airport data",
-       caption = "Data: Aemet.",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        fill = "Tmax") +
   theme_minimal(base_family = "Arial") +
   theme(aspect.ratio = 1/2,
@@ -187,6 +196,9 @@ ace_data %>%
         legend.title = element_text(face = "bold", size = 9, hjust = 0.5),
         panel.border = element_rect(colour = "grey", fill = NA, linewidth = 1)) +
   facet_wrap(vars(month), nrow = 4, ncol = 3, scales = "free")
+
+ggsave("3_Graphs/Max temp calendar.png",
+       plot = last_plot())
 
 
 ace_data %>%
@@ -262,8 +274,8 @@ ace_data %>%
               borderColor = "#e0e0e0",
               cellPadding = "8px"
             )) %>%
-  add_source(source = html("<strong>Table 1.</strong> Number of days with high temperatures.<br>
-                           Data: Aemet."),
+  add_source(source = html("<b>Table 1.</b> Number of days with high temperatures.<br>
+                           Data: Aemet.<br>Made by Oliver Q.R."),
              font_size = 14,
              font_style = "italic",
              font_color = "grey50",
@@ -282,7 +294,7 @@ ace_data %>%
                           .default = week),
          tmin_cat = cut(tmin, c(0, 20, 25, 30, max(tmin)))) %>%
   ggplot(aes(x = weekday, y = -week, fill = tmin_cat)) +
-  geom_tile(colour = "white", size = 0.4) +
+  geom_tile(colour = "white", linewidth = 0.4) +
   geom_text(aes(label = day), size = 2.5) +
   scale_fill_manual(values = c("white", "yellow", "orange", "red"),
                     labels = c("≤20°C", "20-25°C", "25-30°C", ">30°C"),
@@ -290,7 +302,7 @@ ace_data %>%
                                          override.aes = list(colour = "black"))) +
   labs(title = "Daily minimum temperatures throughout the year 2023",
        subtitle = "Lanzarote Airport data",
-       caption = "Data: Aemet.",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        fill = "Tmin") +
   theme_minimal(base_family = "Arial") +
   theme(aspect.ratio = 1/2,
@@ -309,6 +321,9 @@ ace_data %>%
         legend.title = element_text(face = "bold", size = 9, hjust = 0.5),
         panel.border = element_rect(colour = "grey", fill = NA, linewidth = 1)) +
   facet_wrap(vars(month), nrow = 4, ncol = 3, scales = "free")
+
+ggsave("3_Graphs/Min temp calendar.png",
+       plot = last_plot())
 
 
 ace_data %>%
@@ -384,8 +399,8 @@ ace_data %>%
               borderColor = "#e0e0e0",
               cellPadding = "8px"
             )) %>%
-  add_source(source = html("<strong>Table 2.</strong> Number of days according to the min. temperature.<br>
-                           Data: Aemet."),
+  add_source(source = html("<b>Table 2.</b> Number of days according to the min. temperature.<br>
+                           Data: Aemet.<br>Made by Oliver Q.R."),
              font_size = 14,
              font_style = "italic",
              font_color = "grey50",
@@ -419,7 +434,7 @@ ace_data %>%
        the <span style=color:#313695>**1973-2000 reference period**</span>",
        x = "Month",
        y = "Mean temperature (°C)",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_markdown(size = 14, colour = "#5a5a5a"),
@@ -430,6 +445,10 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+
+ggsave("3_Graphs/Monthly mean temp anomaly.png",
+       plot = last_plot())
 
 
 
@@ -499,8 +518,8 @@ ace_data %>%
                                     }
                                   })
             )) %>%
-  add_source(source = html("<strong>Table 3.</strong> Monthly average temperature differences.<br>
-                           Data: Aemet."),
+  add_source(source = html("<b>Table 3.</b> Monthly average temperature differences.<br>
+                           Data: Aemet.<br>Made by Oliver Q.R."),
              font_size = 14,
              font_style = "italic",
              font_color = "grey50",
@@ -518,7 +537,7 @@ ace_data %>%
   ggplot(aes(x = month, y = tmean, group = decade, colour = decade, shape = decade)) +
   geom_line(linewidth = 1.2) +
   geom_point(size = 4) +
-  scale_x_discrete(breaks = 1:12,
+  scale_x_continuous(breaks = 1:12,
                    labels = month.abb) +
   scale_y_continuous(breaks = pretty_breaks(n = 7)) +
   scale_colour_manual(values = c("#313695", "#74add1", "#abd9e9", "#fee090", "#f46d43", "#a50026")) +
@@ -527,7 +546,7 @@ ace_data %>%
        subtitle = "Data from 1973 to 2023",
        x = "Month",
        y = "Temperature (°C)",
-       caption = "Data: Aemet.",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        colour = "Decade",
        shape = "Decade") +
   theme_bw(base_family = "Arial") +
@@ -541,6 +560,9 @@ ace_data %>%
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
 
+
+ggsave("3_Graphs/Monthly mean temp by decade.png",
+       plot = last_plot())
 
 
 # annual precipitation
@@ -564,7 +586,7 @@ ace_data %>%
        subtitle = "Data from 1973 to 2023",
        x = "Year",
        y = "mm",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_text(size = 14, colour = "#5a5a5a"),
@@ -573,6 +595,10 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+
+ggsave("3_Graphs/Annual precipitation.png",
+       plot = last_plot())
 
 
 # yearly precipitation anomalies (reference period: 1973-2000)
@@ -588,16 +614,16 @@ ace_data %>%
   filter(year > 1972 & year < 2024) %>%
   group_by(year) %>%
   summarise(prec = sum(prec, na.rm = TRUE)) %>%
-  mutate(prec_an = prec - ref_prec) %>%
+  mutate(prec_an = (100 * prec / ref_prec) - 100) %>%
   ggplot(aes(x = year, y = prec_an, fill = prec_an)) +
   geom_col(colour = "#333333") +
   geom_label_repel(data = ~.x %>% filter(prec_an == min(prec_an)),
-                   aes(x = year, y = prec_an, label = paste(round(prec_an, 2), "mm", sep = " ")),
+                   aes(x = year, y = prec_an, label = paste0(round(prec_an, 2), "%")),
                    size = 3.5, nudge_x = -0.5, nudge_y = -15, fill = "white",
                    colour = "#67001f", fontface = "bold") +
   geom_label_repel(data = ~.x %>% filter(prec_an == max(prec_an)),
                    aes(x = year, y = prec_an, 
-                       label = paste(paste0("+", round(prec_an, 2)), "mm", sep = " ")),
+                       label = paste0("+", round(prec_an, 2), "%")),
                    size = 3.5, nudge_x = 1, nudge_y = 1, fill = "white",
                    colour = "darkblue", fontface = "bold") +
   scale_x_continuous(breaks = seq(1975, 2020, 5)) +
@@ -609,8 +635,8 @@ ace_data %>%
   labs(title = "Annual precipitation anomalies at Lanzarote Airport",
        subtitle = "Deviations from the annual mean precipitation for the period 1973-2000",
        x = "Year",
-       y = "mm",
-       caption = "Data: Aemet.",
+       y = "Precipitation anomaly (%)",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        fill = "mm") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
@@ -622,6 +648,10 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+
+ggsave("3_Graphs/Prec anomalies.png",
+       plot = last_plot())
 
 
 # monthly precipitation
@@ -650,11 +680,11 @@ ace_data %>%
   scale_fill_manual(values = c("#a6cee3", "#1f78b4"),
                     labels = c("2023", "1973-2000"),
                     name = "Period") +
-  labs(title = "Comparison of monthly precipitation in 2023 vs. the reference period",
+  labs(title = "Monthly precipitation in 2023 vs. the reference period",
        subtitle = "Reference period: 1973-2000",
        x = "Month",
        y = "Precipitation (mm)",
-       caption = "Data: Aemet") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_text(size = 14, colour = "#5a5a5a"),
@@ -667,6 +697,10 @@ ace_data %>%
         panel.grid.minor = element_blank())
 
 
+ggsave("3_Graphs/Monthly prec 2023.png",
+       plot = last_plot())
+
+
 # year classification according to mean temperature and precipitation anomalies
 ace_data %>%
   filter(year > 1972 & year < 2024) %>%
@@ -677,39 +711,39 @@ ace_data %>%
               filter(year > 1972 & year < 2024) %>%
               group_by(year) %>%
               summarise(prec = sum(prec, na.rm = TRUE)) %>%
-              mutate(prec_an = prec - ref_prec),
+              mutate(prec_an = (100 * prec / ref_prec) - 100),
             by = "year") %>%
   mutate(decade = as.character(10 * floor(year / 10))) %>%
   ggplot(aes(x = prec_an, y = tmean_an, shape = decade)) +
-  geom_rect(aes(xmin = -100, xmax = 0, ymin = -1.5, ymax = 0),
+  geom_rect(aes(xmin = -200, xmax = 0, ymin = -1.5, ymax = 0),
             fill = "#a6cee3", alpha = 0.1) +
   geom_rect(aes(xmin = 0, xmax = 200, ymin = -1.5, ymax = 0),
             fill = "#1f78b4", alpha = 0.1) +
   geom_rect(aes(xmin = 0, xmax = 200, ymin = 0, ymax = 2.5),
             fill = "#d73027", alpha = 0.1) +
-  geom_rect(aes(xmin = -100, xmax = 0, ymin = 0, ymax = 2.5),
+  geom_rect(aes(xmin = -200, xmax = 0, ymin = 0, ymax = 2.5),
             fill = "#fdae61", alpha = 0.1) +
-  annotate(geom = "text", label = "Colder\nDrier", x = -95, y = -1.25, colour = "white",
+  annotate(geom = "text", label = "Colder\nDrier", x = -195, y = -1.25, colour = "white",
            hjust = "left", fontface = "bold") +
   annotate(geom = "text", label = "Colder\nWetter", x = 195, y = -1.25, colour = "white",
            hjust = "right", fontface = "bold") +
   annotate(geom = "text", label = "Warmer\nWetter", x = 195, y = 2.25, colour = "white",
            hjust = "right", fontface = "bold") +
-  annotate(geom = "text", label = "Warmer\nDrier", x = -95, y = 2.25, colour = "white",
+  annotate(geom = "text", label = "Warmer\nDrier", x = -195, y = 2.25, colour = "white",
            hjust = "left", fontface = "bold") +
-  geom_label_repel(data = ~.x %>% filter(year %in% 2019:2023),
+  geom_label_repel(data = ~.x %>% filter(year == 2023),
                    aes(label = year), fontface = "bold") +
   geom_point(size = 3) +
-  scale_x_continuous(limits = c(-100, 200),
+  scale_x_continuous(limits = c(-200, 200),
                      expand = c(0, 0)) +
   scale_y_continuous(limits = c(-1.5, 2.5),
                      expand = c(0, 0)) +
   scale_shape_manual(values = 20:15) +
   labs(title = "Temperature and precipitation anomalies at Lanzarote airport",
        subtitle = "Data from 1973-2023. Reference period: 1973-2000",
-       x = "Precipitation anomaly (mm)",
+       x = "Precipitation anomaly (%)",
        y = "Temperature anomaly (°C)",
-       caption = "Data: Aemet.",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
        shape = "Decade") +
   theme_bw(base_family = "sans") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
@@ -721,6 +755,13 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+
+ggsave("3_Graphs/Prec temp anomalies.png",
+       plot = last_plot())
+
+## gganimate
+  
 
 
 # What about 2024 (first 9 months)
@@ -767,7 +808,7 @@ ace_data %>%
        with the <span style=color:#313695>**1973-2000 reference period**</span>",
        x = "Day",
        y = "Mean temperature (°C)",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_markdown(size = 14, colour = "#5a5a5a"),
@@ -778,6 +819,9 @@ ace_data %>%
         axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
         panel.grid.major = element_line(colour = "grey90"),
         panel.grid.minor = element_blank())
+
+ggsave("3_Graphs/Daily mean temp 2024.png",
+       plot = last_plot())
 
 
 # 2024 annual mean temperature (9 first months)
@@ -791,17 +835,20 @@ ace_data %>%
   geom_point(size = 3, colour = "#1f78b4") +
   geom_label_repel(data = ~.x %>% filter(tmean == min(tmean)),
                    aes(x = year, y = tmean, label = paste0(round(tmean, 2), "°C")),
-                   size = 3.5, nudge_x = 3, colour ="#313695", fontface = "bold") +
+                   size = 3.5, nudge_x = 3, colour ="#313695", alpha = 0.5) +
   geom_label_repel(data = ~.x %>% filter(tmean == max(tmean)),
                    aes(x = year, y = tmean, label = paste0(round(tmean, 2), "°C")),
-                   size = 3.5, nudge_x = -3, colour ="#a50026", fontface = "bold") +
+                   size = 3.5, nudge_x = -3, colour ="#a50026", alpha = 0.5) +
+  geom_label_repel(data = ~.x %>% filter(year == 2024),
+                   aes(x = year, y = tmean, label = paste0(round(tmean, 2), "°C")),
+                   size = 3.5, nudge_y = -0.5, colour ="#a50026", fontface = "bold") +
   scale_y_continuous(breaks = pretty_breaks(n = 10)) +
   scale_x_continuous(breaks = seq(1975, 2020, 5)) +
-  labs(title = "Annual mean temperature of the first 9 months of the year at Lanzarote Airport",
+  labs(title = "Annual mean temperature at Lanzarote Airport (Jan to Sept)",
        subtitle = "Data from 1973 to 2024",
        x = "Year",
        y = "Mean temperature (°C)",
-       caption = "Data: Aemet.") +
+       caption = "Data: Aemet.\nMade by Oliver Q.R.") +
   theme_bw(base_family = "Arial") +
   theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
         plot.subtitle = element_text(size = 14, colour = "#5a5a5a"),
@@ -812,6 +859,67 @@ ace_data %>%
         panel.grid.minor = element_blank())
 
 
+ggsave("3_Graphs/Mean temp 2024.png",
+       plot = last_plot())
+
+
+# Precipitation in 2024
+# yearly precipitation anomalies (reference period: 1973-2000)
+ref_prec <- ace_data %>%
+  filter(year > 1972 & year < 2001) %>%
+  group_by(year) %>%
+  summarise(prec = sum(prec, na.rm = TRUE)) %>%
+  summarise(prec = mean(prec, na.rm = TRUE)) %>%
+  pull()
+
+
+ace_data %>%
+  filter(year > 1972,
+         month %in% 1:9) %>%
+  group_by(year) %>%
+  summarise(prec = sum(prec, na.rm = TRUE)) %>%
+  mutate(prec_an = (100 * prec / ref_prec) - 100) %>%
+  ggplot(aes(x = year, y = prec_an, fill = prec_an)) +
+  geom_col(colour = "#333333") +
+  geom_label_repel(data = ~.x %>% filter(prec_an == min(prec_an)),
+                   aes(x = year, y = prec_an, label = paste0(round(prec_an, 2), "%")),
+                   size = 3.5, nudge_x = -0.5, nudge_y = -25, fill = "white",
+                   colour = "#67001f", alpha = 0.5) +
+  geom_label_repel(data = ~.x %>% filter(prec_an == max(prec_an)),
+                   aes(x = year, y = prec_an, 
+                       label = paste0("+", round(prec_an, 2), "%")),
+                   size = 3.5, nudge_x = 1, nudge_y = 1, fill = "white",
+                   colour = "darkblue", alpha = 0.5) +
+  geom_label_repel(data = ~.x %>% filter(year == 2024),
+                   aes(x = year, y = prec_an, label = paste0(round(prec_an, 2), "%")),
+                   size = 3.5, nudge_x = -0.5, nudge_y = -25, fill = "white",
+                   colour = "#67001f", fontface = "bold") +
+  scale_x_continuous(breaks = seq(1975, 2020, 5)) +
+  scale_y_continuous(limits = c(-200, 200),
+                     breaks = pretty_breaks(n = 9)) +
+  scale_fill_gradient2(low = "#9e0142", mid = "#f7f7f7", high = "#5e4fa2",
+                       midpoint = 0,
+                       limits = c(-200, 200)) +
+  labs(title = "Precipitation anomalies at Lanzarote Airport from Jan to Sept",
+       subtitle = "Deviations from the annual mean precipitation for the period 1973-2000",
+       x = "Year",
+       y = "Precipitation anomaly (%)",
+       caption = "Data: Aemet.\nMade by Oliver Q.R.",
+       fill = "mm") +
+  theme_bw(base_family = "Arial") +
+  theme(plot.title = element_text(face = "bold", size = 20, colour = "#003080"),
+        plot.subtitle = element_text(size = 14, colour = "#5a5a5a"),
+        plot.caption = element_text(size = 10, face = "italic", colour = "grey50"),
+        legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10),
+        axis.title = element_text(face = "bold", colour = "#003080"),
+        axis.text = element_text(size = 12, face = "bold", colour = "grey30"),
+        panel.grid.major = element_line(colour = "grey90"),
+        panel.grid.minor = element_blank())
+
+
+ggsave("3_Graphs/Prec anomalies jan-sept.png",
+       plot = last_plot())
 
 
 
